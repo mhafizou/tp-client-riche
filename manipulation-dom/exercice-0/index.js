28f4b3af907a8ce0515de5597744998731ba45ec
Jérôme Cessou <jerome.cessou@rca.fr>
2021-01-21 11:09:01 +0100

    Exercice 0 V2 : en querySelectorAll

--- manipulation-dom/exercice-0/index.js
+++ manipulation-dom/exercice-0/index.js
@@ -1,7 +1,7 @@
 function LoadScript(){
   var img1 = document.getElementById('img1');
   var img2 = document.getElementById('img2');
-
+  
   img1.addEventListener('click', ClickImg);
   
   img2.addEventListener('click', ClickImg);
@@ -11,4 +11,15 @@ function ClickImg (Event){
   var imgResultat = document.getElementById('imgResultat');
 
   imgResultat.setAttribute('src', Event.target.getAttribute('src'));
+}
+
+function LoadScriptV2(){
+  const images = document.querySelectorAll('#img1, #img2');
+  const imgResultat = document.querySelector('#imgResultat');
+
+  for( const image of images){
+    image.addEventListener('click', function (Event){
+      imgResultat.setAttribute('src', Event.target.getAttribute('src'));
+    });  
+  }
 }
