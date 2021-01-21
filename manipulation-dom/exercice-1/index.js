40900ed29e62e520008e2a819edfeb2483cf61ea
Jérôme Cessou <jerome.cessou@rca.fr>
2021-01-21 12:44:37 +0100

    Exercice 1

--- manipulation-dom/exercice-1/index.js
+++ manipulation-dom/exercice-1/index.js
@@ -1,1 +1,21 @@
-// CODE JAVASCRIPT
+function showModal(){
+  const modalBackground = document.querySelector('#modal_background');
+  const modalContent = document.querySelector('#modal_content');
+  
+  modalBackground.classList.remove('displayNone');
+  modalBackground.classList.add('displayBlock');
+  
+  modalBackground.addEventListener('click',closeModal);
+}
+
+function closeModal(Event){
+  const modalBackground = document.querySelector('#modal_background');
+  const modalContent = document.querySelector('#modal_content');
+  
+  if ((!Event) || (Event.target ==  modalBackground)) {
+    modalBackground.classList.remove('displayBlock');
+    modalBackground.classList.add('displayNone');
+
+    modalBackground.removeEventListener('click',closeModal);
+  }
+}
