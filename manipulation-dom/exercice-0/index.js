2c8328324001d351af8da1556e7f13f857e978e6
Jérôme Cessou <jerome.cessou@rca.fr>
2021-01-21 11:08:19 +0100

    Exercice 0 V1 : en GetElementById

--- manipulation-dom/exercice-0/index.js
+++ manipulation-dom/exercice-0/index.js
@@ -1,1 +1,14 @@
-// CODE JAVASCRIPT
+function LoadScript(){
+  var img1 = document.getElementById('img1');
+  var img2 = document.getElementById('img2');
+
+  img1.addEventListener('click', ClickImg);
+  
+  img2.addEventListener('click', ClickImg);
+}
+
+function ClickImg (Event){
+  var imgResultat = document.getElementById('imgResultat');
+
+  imgResultat.setAttribute('src', Event.target.getAttribute('src'));
+}
